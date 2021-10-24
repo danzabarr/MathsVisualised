d793dfaeeb6a3173e82d017a5faec98cbdb22b13
danzabarr <danzabarr@hotmail.co.uk>
2021-10-24 12:50:11 +0100

    Update ComplexPlane.py

--- ComplexPlane/ComplexPlane/ComplexPlane.py
+++ ComplexPlane/ComplexPlane/ComplexPlane.py
@@ -1,9 +1,40 @@
+import math
 import tkinter as tk
+from enum import Enum
+
+
+
+def mag(x, y):
+	return math.sqrt(x * x + y * y)
+
+def sf(x, digits):
+	return round(x, digits - int(math.floor(math.log10(abs(x)))) - 1)
+
+class Angle(Enum):
+	DEGREES = 1
+	RADIANS = 2
+	PI_RADIANS = 3
+
+def formatAngle(degrees, angleFormat=Angle.DEGREES, sigfigs=3):
+	if angleFormat == Angle.DEGREES:
+		angle = sf(degrees, 3)
+		return f'{angle}°'
+	
+	if angleFormat == Angle.RADIANS:
+		angle = sf(math.radians(degrees), sigfigs)
+		return f'{angle} rad'
+
+	if angleFormat == Angle.PI_RADIANS:
+		angle = sf(math.radians(degrees) / math.pi, sigfigs)
+		return f'{angle}π'
+
+	return ""
+
 
 width = 1000
 height = 600
 origin = width / 2, height / 2
-scale = 10, 10
+scale = 20, 20
 
 window = tk.Tk()
 canvas = tk.Canvas(window, width=width, height=height)
@@ -23,16 +54,77 @@ def draw_axes():
 
 	canvas.create_line(0, -height / 2, 0, height / 2, arrow=tk.BOTH)
 
-def draw_vector(x1, y1, x2, y2, color):
-	canvas.create_line(x1 * scale[0], -y1 * scale[1], x2 * scale[0], -y2 * scale[1], fill=color, arrow=tk.LAST)
+def draw_arc(degrees, color):
+	canvas.create_arc(-100, -100, 100, 100, extent=degrees, style=tk.ARC)
 
+def draw_vector_arc(x, y, color):
 
-def draw_vector(x, y, color):
-	canvas.create_line(0, 0, x * scale[0], -y * scale[1], fill=color, arrow=tk.LAST)
-	canvas.create_text(x * scale[0], -y * scale[1], fill=color, text=f'({x},{y})', anchor=tk.S)
+	xS = x * scale[0]
+	yS = -y * scale[1]
+
+	radians = math.atan2(y, x)
+	degrees = math.degrees(radians)
+	magnitude = mag(xS, yS)
+
+	canvas.create_arc(-magnitude, -magnitude, magnitude, magnitude, extent=degrees, style=tk.ARC, outline=color)
+	canvas.create_line(0, 0, xS, yS, fill=color, arrow=tk.LAST)
+	canvas.create_text(x * scale[0], -y * scale[1], fill=color, text=f'{sf(x, 3)}+{sf(y, 3)}i ({formatAngle(degrees)})', anchor=tk.S)
+	
+def draw_vector_arc2(degrees, magnitude, color, start=0):
+	radians = math.radians(degrees)
+	startRadians = math.radians(start)
+	x = math.cos(radians) * magnitude
+	y = math.sin(radians) * magnitude
+	xS = math.cos(radians + startRadians) * magnitude * scale[0]
+	yS = -math.sin(radians + startRadians) * magnitude * scale[1]
+	magS = mag(xS, yS)
+
+	canvas.create_arc(-magS, -magS, magS, magS, start=start, extent=degrees, style=tk.ARC, outline=color)
+	canvas.create_line(0, 0, xS, yS, fill=color, arrow=tk.LAST)
+	canvas.create_text(x * scale[0], -y * scale[1], fill=color, text=f'{sf(x, 3)}+{sf(y, 3)}i ({formatAngle(degrees)} x {sf(magnitude, 3)})', anchor=tk.S)
+		
+class Complex:
+
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+		self.magnitude = mag(a, b)
+		self.angle = math.atan2(b, a)
+
+	def degrees(self):
+		return math.degrees(self.angle)
+
+	def mag_angle(magnitude, radians):
+		return Complex(math.cos(radians) * magnitude, math.sin(radians) * magnitude)
+
+	def __add__(self, other):
+		return Complex(self.a + other.a, self.b + other.b)
+
+	def __mul__(self, other):
+		radians = self.angle + other.angle
+		magnitude = self.magnitude * other.magnitude
+		return Complex(math.cos(radians) * magnitude, math.sin(radians) * magnitude)
+
+	def print(self):
+		print(f'{sf(self.a, 3)}+{sf(self.b, 3)}i ({formatAngle(math.degrees(self.angle))} x {sf(self.magnitude, 3)})')
+
+	def draw(self, color, start=0):
+		draw_vector_arc2(math.degrees(self.angle), self.magnitude, color, start)
 
 draw_axes()
 
-draw_vector(5, 3, "red")
+c1 = Complex(4, -1)
+c2 = Complex(-3, 2)
+c3 = c1 * c2
+
+c1.print()
+print("x")
+c2.print()
+print("=")
+c3.print()
+
+c1.draw("red")
+c2.draw("green", start=c1.degrees())
+c3.draw("blue")
 
 window.mainloop()
