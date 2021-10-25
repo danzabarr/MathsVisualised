e72a9eeb618efbe4f17995ce135b1aa95f6f951a
danzabarr <danzabarr@hotmail.co.uk>
2021-10-25 11:37:53 +0100

    Euler's formula
    Visualise euler's formula e^(iθ) for an increasing value of θ as it traces the unit circle on the complex plane using the sum of the polynomial expansion 1 + iθ + 1/2(iθ)^2 + 1/6(iθ)^3 + 1/24(iθ)^4....etc and showing that e^(iθ) = cosθ + isinθ

--- ComplexPlane/ComplexPlane/ComplexPlane.py
+++ ComplexPlane/ComplexPlane/ComplexPlane.py
@@ -1,5 +1,6 @@
 import math
 import tkinter as tk
+import time
 from enum import Enum
 
 def mag(x, y):
@@ -44,7 +45,7 @@ width = 1200
 height = 800
 
 #Scale of the graph in pixels
-scale = 30
+scale = 300
 
 #Create tkinter window and canvas
 window = tk.Tk()
@@ -54,7 +55,7 @@ canvas.configure(scrollregion=(-width / 2, -height / 2, width / 2, height / 2))
 
 def draw_axes():
 	"""
-	Draw the graph axes and grid lines
+	Draw the graph axes, unit circle and grid lines
 	"""
 
 	for x in range((int) (scale), (int) (width / 2), (int) (scale)):
@@ -65,12 +66,13 @@ def draw_axes():
 		canvas.create_line(-width / 2, y, width / 2, y, fill="light gray")
 		canvas.create_line(-width / 2, -y, width / 2, -y, fill="light gray")
 
-	canvas.create_line(-width / 2, 0, width / 2, 0, arrow=tk.BOTH)
+	canvas.create_oval(-scale, -scale, scale, scale, outline="light gray")
 
+	canvas.create_line(-width / 2, 0, width / 2, 0, arrow=tk.BOTH)
 	canvas.create_line(0, -height / 2, 0, height / 2, arrow=tk.BOTH)
 
 
-def draw_vector_arc(x, y, color):
+def draw_vector_arc(x, y, color, tags="draw"):
 
 	xS = x * scale
 	yS = -y * scale
@@ -79,11 +81,11 @@ def draw_vector_arc(x, y, color):
 	degrees = math.degrees(radians)
 	magnitude = mag(xS, yS)
 
-	canvas.create_arc(-magnitude, -magnitude, magnitude, magnitude, extent=degrees, style=tk.ARC, outline=color)
-	canvas.create_line(0, 0, xS, yS, fill=color, arrow=tk.LAST)
-	canvas.create_text(x * scale, -y * scale, fill=color, text=f'{sf(x, 3)}+{sf(y, 3)}i ({formatAngle(degrees)})', anchor=tk.S)
+	canvas.create_arc(-magnitude, -magnitude, magnitude, magnitude, extent=degrees, style=tk.ARC, outline=color, tags=tags)
+	canvas.create_line(0, 0, xS, yS, fill=color, arrow=tk.LAST, tags=tags)
+	canvas.create_text(x * scale, -y * scale, fill=color, text=f'{sf(x, 3)}+{sf(y, 3)}i ({formatAngle(degrees)})', anchor=tk.S, tags=tags)
 	
-def draw_vector_arc2(degrees, magnitude, color, start=0):
+def draw_vector_arc2(degrees, magnitude, color, start=0, tags="draw"):
 	radians = math.radians(degrees)
 	startRadians = math.radians(start)
 	x = math.cos(radians) * magnitude
@@ -92,10 +94,44 @@ def draw_vector_arc2(degrees, magnitude, color, start=0):
 	yS = -math.sin(radians + startRadians) * magnitude * scale
 	magS = mag(xS, yS)
 
-	canvas.create_arc(-magS, -magS, magS, magS, start=start, extent=degrees, style=tk.ARC, outline=color)
-	canvas.create_line(0, 0, xS, yS, fill=color, arrow=tk.LAST)
-	canvas.create_text(x * scale, -y * scale, fill=color, text=f'{sf(x, 3)}+{sf(y, 3)}i ({formatAngle(degrees)} x {sf(magnitude, 3)})', anchor=tk.S)
+	canvas.create_arc(-magS, -magS, magS, magS, start=start, extent=degrees, style=tk.ARC, outline=color, tags=tags)
+	canvas.create_line(0, 0, xS, yS, fill=color, arrow=tk.LAST, tags=tags)
+	#canvas.create_text(x * scale, -y * scale, fill=color, text=f'{sf(x, 3)}+{sf(y, 3)}i ({formatAngle(degrees)} x {sf(magnitude, 3)})', anchor=tk.S, tags=tags)
 		
+
+def exp(x, r=100):
+	return sum([
+		x**n / math.factorial(n)	
+		for n in range(0, r)
+	])
+
+def draw_exp_spiral(theta, magnitude=1, start=0, end=100):
+
+	v0 = (0, 0)
+	d0 = v0
+	gr = 1 / 1.61803398875
+
+	for n in range(start, end):
+		r = ((n + 1) % 2) * (2 - ((n + 1) % 4))
+		i = (n % 2) * (2 - (n % 4))
+		
+		f = 1 / math.factorial(n)
+		p = theta ** n
+
+		r *= f * p * magnitude * scale
+		i *= f * p * magnitude * scale
+
+		v1 = (r + v0[0], -i + v0[1])
+
+		d1 = (r * gr + v0[0], -i * gr + v0[1])
+
+		canvas.create_line(v0, v1, tags="draw")
+		#canvas.create_arc(min(vm1[0], v1[0]), min(vm1[1], v1[1]), max(vm1[0], v1[0]), max(vm1[1], v1[1]), tags="draw")
+		canvas.create_line(d0, v0, d1, smooth="true", tags="draw")
+		v0 = v1
+		d0 = d1
+
+
 class Complex:
 
 	def __init__(self, a, b):
@@ -142,14 +178,25 @@ class Complex:
 	def draw(self, color, start=0):
 		draw_vector_arc2(self.degrees(), self.magnitude(), color, start)
 
+
+
+
+
+#c1 = Complex(4, 1)
+#c2 = Complex(3, 2)
+#c3 = c1 * c2
+#
+#c1.draw("red")
+#c2.draw("green", start=c1.degrees())
+#c3.draw("blue")
+
 draw_axes()
 
-c1 = Complex(4, 1)
-c2 = Complex(3, 2)
-c3 = c1 * c2
-
-c1.draw("red")
-c2.draw("green", start=c1.degrees())
-c3.draw("blue")
-
-window.mainloop()
+a = 0
+while True:
+	canvas.delete('draw')
+	draw_vector_arc2(math.degrees(a), 1, "green")
+	draw_exp_spiral(a, 1, 0, 100)
+	a += math.pi * .01
+	time.sleep(0.01)
+	canvas.update()
