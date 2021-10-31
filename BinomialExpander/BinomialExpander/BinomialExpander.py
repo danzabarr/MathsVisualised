7438d0426b92b44154b8c4d52333f26b54ac981f
danzabarr <danzabarr@hotmail.co.uk>
2021-10-31 17:53:18 +0000

    Update BinomialExpander.py

--- BinomialExpander/BinomialExpander/BinomialExpander.py
+++ BinomialExpander/BinomialExpander/BinomialExpander.py
@@ -1,24 +1,6 @@
 from fractions import Fraction
 import math
 
-terms = 5
-
-# Input binomial in the form
-# (constant + x_coefficient * x)^exponent
-# Example
-# (4 + 3x)^3
-constant = Fraction(input("Enter constant: "))
-x_coefficient = Fraction(input("Enter x coefficient: "))
-exponent = Fraction(input("Enter exponent: "))
-x = Fraction(input("Enter a value for x: "))
-
-# rearrange to the form:
-# constant^exponent(1 + x_coefficient / constant * x)^exponent
-# by factoring out the constant and applying law of indices (ab)^x = a^x * b^x
-factor = Fraction(constant ** exponent)
-coefficient = Fraction(x_coefficient, constant)
-
-sum = 0
 
 def prod(list):
     p = 1
@@ -47,57 +29,83 @@ def format_fraction_exponent(fraction, exponent):
     
     return f'({format_fraction(fraction)})x^{exponent}'
 
+# Input binomial in the form
+# (constant + x_coefficient * x)^exponent
+# Example
+# (4 + 3x)^3
+constant = Fraction(input("Enter constant: "))
+x_coefficient = Fraction(input("Enter x coefficient: "))
+exponent = Fraction(input("Enter exponent: "))
+terms = int(input("Enter a maximum number of terms: "))
+x = Fraction(input("Enter a value for x: "))
+
+# rearrange to the form:
+# constant^exponent(1 + x_coefficient / constant * x)^exponent
+# by factoring out the constant and applying law of indices (ab)^x = a^x * b^x
+factor = Fraction(constant ** exponent)
+coefficient = Fraction(x_coefficient, constant)
 
 print()
+print("Binomial:")
 print(f'({constant} + {x_coefficient}x)^{exponent}')
-print(f'= {factor}(1 + {coefficient}x)^{exponent}')
+print()
+print("Factorised:")
+print(f'={factor}(1 + {coefficient}x)^{exponent}')
 print()
 
-expansion = "= "
-
-for term in range(0, terms):
+def expand(terms, verbose):
+    sum = 0
+    expansion = "="
+    for term in range(0, terms):
     
-    one_over_n_factorial = Fraction(1, math.factorial(term))
-    n_values = [exponent - n for n in range(0, term)]
-    x_value = coefficient ** term
-    term_coefficient = factor * one_over_n_factorial * prod(n_values) * x_value
+        one_over_n_factorial = Fraction(1, math.factorial(term))
+        n_values = [exponent - n for n in range(0, term)]
+        #Add to the sum where x = a value
+        term_coefficient = factor * one_over_n_factorial * prod(n_values) * coefficient ** term
     
-    #Break if the sequence terminates
-    if (term_coefficient == 0):
-        break
+        #Break if the sequence terminates
+        if (term_coefficient == 0):
+            break
 
-    #Add to the sum where x = a value
-    sum += term_coefficient * x
-
-
-    print("TERM #" + str(term + 1))
-
-    if (factor != 1):
-        print(f'{factor}(', end='')
-
-    if (one_over_n_factorial != 1):
-        print(format_fraction(one_over_n_factorial), end='')
+        sum += term_coefficient * (x ** term)
     
-    for i in n_values:
-        print(f'({format_fraction(i)})', end='')
+        if (verbose):
+            print("TERM #" + str(term + 1))
 
-    print(format_fraction_exponent(x_value, term), end='')
+            print("=", end='')
+
+            if (factor != 1):
+                print(f'{factor}(', end='')
+
+            if (one_over_n_factorial != 1):
+                print(format_fraction(one_over_n_factorial), end='')
     
-    if (factor != 1):
-        print(')', end='')
+            for i in n_values:
+                print(f'({format_fraction(i)})', end='')
 
+            print(format_fraction_exponent(coefficient ** term, term), end='')
+    
+            if (factor != 1):
+                print(')', end='')
+
+            print()
+
+        term_string = format_fraction_exponent(term_coefficient, term)
+        if (verbose):
+            print('=' + term_string)
+
+        if (term > 0):
+            expansion += "+"
+        expansion += term_string
+        print()
+
+    print("Expanded Polynomial: ")
+    print(expansion)
+    print()
+    print(f"Sum (where x = {x}):")
+    print(f'={format_fraction(sum)} ({sum.numerator / sum.denominator})')
     print()
 
-    term_string = format_fraction_exponent(term_coefficient, term)
-    print(term_string)
-    if (term > 0):
-        expansion += "+"
-    expansion += term_string
+#for n in range(1, terms):
+expand(terms, True)
 
-    print()
-
-print()
-print(expansion)
-print()
-print(f'{format_fraction(sum)} where x = {x}')
-print()
